4c94733dc6cab2fcc40f7e49fef3e695b85f0518
Laurent Lacôte <laurent.lacote@private.com>
2026-07-23 15:03:00 +0200

    Draft: ORM - First part of Stock model #18
    Missing quantity, will be in separate commit
      to show the related dependencies.

--- backoffice/models.py
+++ backoffice/models.py
@@ -11,6 +11,8 @@ from sqlalchemy.orm import mapped_column    # Required to "configure" a column
 
 # "SQL Data types"
 from sqlalchemy import String
+# SQL Constraints
+from sqlalchemy import ForeignKey, PrimaryKeyConstraint
 
 
 class Base(DeclarativeBase):
@@ -23,3 +25,20 @@ class Branch(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     label: Mapped[str] = mapped_column(String(50), unique=True)
+
+
+class Stock(Base):
+    """Defines model for branches's stocks"""
+    __tablename__ = "stocks"
+    # Reminder: "just the type" implies "NOT NULL" (otherwise [int | None])
+    branch_id: Mapped[int] = mapped_column(ForeignKey("branches.id"))
+    product_id: Mapped[int]
+    # NOTE: could also define "combined primary key" by just directly adding
+    #  'primary_key=True' as another option of mapped_column() in both columns
+    #  ex product_id: Mapped[int] = mapped_column(default=0, primary_key=True)
+    # BUT in that case the column used to index the "combined PK" would always
+    #   be the FIRST column declared. Meanwhile, the explicit syntax belows
+    #   lets dev choose which to use whatever happens.
+    __table_args__ = (
+        PrimaryKeyConstraint("product_id", "branch_id"),
+    )
