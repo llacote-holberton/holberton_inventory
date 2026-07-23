c2368b6b65025101747e93a439de8512387a9dc4
Laurent Lacôte <laurent.lacote@private.com>
2026-07-23 16:45:00 +0200

    Feat: ORM - complete Users model #18
    NOTE: kept several inline comments
      explaining concepts to ease up review
      and concepts assimilation.
    
    May remove them in a final "cleaning pass"
      before 1.0

--- backoffice/models.py
+++ backoffice/models.py
@@ -45,3 +45,46 @@ class Stock(Base):
         CheckConstraint("quantity >=0", name="positive_stock"),
         Index("idx_stocks_by_pid", "product_id"),  # Explicit index name
     )
+
+
+import enum  # Import put here for now to stress relation with User model.
+# Apparently in Alchemy 2.0 no need to import the related Alchemy Enum anymore
+# from sqlalchemy import Enum
+# https://docs.sqlalchemy.org/en/20/orm/declarative_tables.html
+#   #using-python-enum-or-pep-586-literal-types-in-the-type-map
+
+class UserRole(str, enum.Enum):  # FIXME check that weird syntax how it works
+    ADMIN = "admin"
+    MANAGER = "manager"
+# This weird syntax is called "double inheritance" (confer lessons on classes)
+# UserRole FIRST inherits from core String class (so gets all related methods)
+# THEN from Enum gets the automatic validation that when trying to instanciate
+#   the provided value matches one of the class attributes (fail -> ValueError)
+# The double inheritance allows things like UserRole.ADMIN == "admin" -> True
+# Otherwise just Enum would make properties be "only Enum objects" making
+#   things like "direct string comparison" impossible.
+# To instanciate, best practice vs "less robust practice"
+# new_user = User(name="Alice", role=UserRole.ADMIN)
+# new_user = User(name="Bob", role="manager")
+# Best practice is using the enum "keys" whenever possible.
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    # Note: "int" + "primary_key" => "auto_increment" is implicit.
+    # ONLY true for primary_key made of a single INT column.
+    id: Mapped[int] = mapped_column(primary_key = True)
+    # Dissociating "property name in Python" from "column name in SQL engine"
+    #   to have similar "id" between Models and avoid redundancy (User.user_id)
+    name: Mapped[str] = mapped_column("user_name", String(30), unique=True)
+    # Note: no need to add specific UniqueConstraint with import UNLESS we would
+    #   want an arbitrary name like for above Index or if we needed to "compose"
+    #   the Unicity check upon several columns at once.
+    password_hash: Mapped[str] = mapped_column(String(255))
+    role: Mapped[UserRole] = mapped_column(default=UserRole.MANAGER)
+    branch_id: Mapped[int|None] = mapped_column(ForeignKey("branches.id"))
+    # Same as for enum, with Alchemy 2.0 native Python core data types are
+    #   automatically "translated" into the inner Alchemy class (here Boolean)
+    is_active: Mapped[bool]
+
