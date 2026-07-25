b6caf569b22321f7fd5a33913d2cc9a341b0bca0
Laurent Lacôte <laurent.lacote@private.com>
2026-07-25 19:30:00 +0200

    Feat: CRUD - remove_stock function #19

--- backoffice/crud.py
+++ backoffice/crud.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 """Module dedicated to actual CRUD operations on database"""
 from sqlalchemy.orm import Session
+from sqlalchemy import update
 from sqlalchemy.dialects.mysql import insert as mysql_insert
 from models import Stock
 
@@ -48,6 +49,48 @@ def add_stock(db: Session, *, product_id: int, branch_id: int, amount: int) -> i
     # Step 2: returning updated amount by reading it from the table (as we
     #   never read it before updating + another write could have happened)
     return (get_stock(db, product_id=product_id, branch_id=branch_id)).quantity
+
+# Required because...
+# 1/ We want to keep signature of remove_stock simple, inline with others
+#     so just return "int" as "quantity post-update".
+#   A dict with "success: bool, message: reason, quantity: int|None"
+#     would mix up responsabilities between CRUD and API.
+# BUT just returning "Int or None" would leave a compromise on failure case.
+#   "None because no stock row in the first place"?
+#   OR "Not enough existing stock to fulfill the remove request"
+class InsufficientStockError(Exception):
+    def __init__(self, available: int):
+        self.available = available
+        super().__init__(f"insufficient stock: {available} available")
+
+
+def remove_stock(db: Session, branch_id: int,
+                 product_id: int, amount: int) -> int | None:
+    result = db.execute(
+        update(Stock)
+        .where(Stock.branch_id == branch_id, Stock.product_id == product_id,
+                # Essential "defensive condition" to prevent trying to make a
+                #   substraction which would end with a constraint violation
+                #   of "quantity must not be below 0".
+                # And because we decide to reject query instead of "altering"
+                #   to "reduce up to 0" it is the best way.
+                # As if there is not enough quantity in stock then there will
+                #   be 0 row to update...
+                # So not even any need to anticipate a potential rollback.
+                Stock.quantity >= amount)
+        .values(quantity=Stock.quantity - amount)
+    )
+    db.commit()
+    stock = get_stock(db, product_id=product_id, branch_id=branch_id)
+    # Optimal: row exists, quantity was enough for substraction.
+    if result.rowcount > 0:
+        return get_stock(db, product_id=product_id,
+                         branch_id=branch_id).quantity
+    # Business failure 1: no preexisting stock for product in branch
+    if stock is None:
+        return None
+    # Business failure 2: removal request beyond available stock
+    raise InsufficientStockError(available=stock.quantity)
 
 
 # WARNING: ONLY USE FOR INTERNAL TESTS, do NOT EXPOSE.
@@ -115,8 +158,17 @@ if __name__ == "__main__":
         # Test adds new line
         set_stock(db, product_id=35, branch_id=5, quantity=500)
 
+        # === REMOVE OPERATIONS ===
+        # Reduce stock just created from 500 to 400
+        remove_stock(db, product_id=35, branch_id=5, amount=100)
+        # Then back to exactly 0 (still valid operation)
+        remove_stock(db, product_id=35, branch_id=5, amount=400)
 
-    #except Exception as e:
-    #    print(e)
+        # Attempt to reduce by too big of an amount
+        remove_stock(db, product_id=6, branch_id=4, amount=9999)
+        # Attempt to reduce inexisting stock
+        remove_stock(db, product_id=666, branch_id=666, amount=666)
+    except Exception as e:
+        print(e)
     finally:
         db.close()
