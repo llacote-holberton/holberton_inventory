91e2201899f947b7d27f4235af73e07f3400750f
Laurent Lacôte <laurent.lacote@private.com>
2026-07-26 16:08:28 +0200

    Feat: Backoffice API - Login route (INCOMPLETE) #8 #9
    Adds a "login" route which checks username exists
      and has provided valid password.
    
    MISSING: returning a Web Json Token so user can interact
      with database in further requests, instead of just a message.
    Will be in next commit.

--- backoffice/backoffice_api.py
+++ backoffice/backoffice_api.py
@@ -15,7 +15,22 @@ def list_users_route(db: Session = Depends(get_db)):
     # ...mais FastAPI ne sérialise QUE les champs déclarés dans UserOut, le hash est ignoré
 
 
-
+from auth import verify_password
+@app.post("/login")
+def login(username: str, password: str, db: Session = Depends(get_db)):
+    user = crud.get_user_by_name(db, username)
+    # NOTE: it's a "best practice" in security to NOT detail WHY login failed.
+    #   as telling "wrong password" (implied: good login) for example would
+    #   give valuable information to attackers.
+    if user is None or not user.is_active:
+        raise HTTPException(status_code=401, detail="invalid_credentials")
+    if not verify_password(password, user.password_hash):
+        raise HTTPException(status_code=401, detail="invalid_credentials")
+    # Password checks out we can generate a Json Web Token (JWT)
+    #   which can be stored in user's browser to be reused automagically later
+    #   when user will make requests to interact with database.
+    #FIXME replace with JWT generation
+    return {"msg": "Yay! Your password has been successfully validated!"}
 
 # @app.post("/branches/{branch_id}/stock/add")
 # def add_stock_route(
