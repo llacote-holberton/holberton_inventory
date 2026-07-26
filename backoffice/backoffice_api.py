da37fefd46730d463a4265ef80257f09c423fbef
Laurent Lacôte <laurent.lacote@private.com>
2026-07-26 15:23:00 +0200

    Fix: commenting untested unfinished api endpoint #24
    Forgot to remove or test it.

--- backoffice/backoffice_api.py
+++ backoffice/backoffice_api.py
@@ -15,12 +15,14 @@ def list_users_route(db: Session = Depends(get_db)):
     # ...mais FastAPI ne sérialise QUE les champs déclarés dans UserOut, le hash est ignoré
 
 
-@app.post("/branches/{branch_id}/stock/add")
-def add_stock_route(
-    branch_id: int,
-    product_id: int,
-    amount: int,
-    db: Session = Depends(get_db),
-):
-    new_quantity = crud.add_stock(db, product_id=product_id, branch_id=branch_id, amount=amount)
-    return {"branch_id": branch_id, "product_id": product_id, "quantity": new_quantity}
+
+
+# @app.post("/branches/{branch_id}/stock/add")
+# def add_stock_route(
+#     branch_id: int,
+#     product_id: int,
+#     amount: int,
+#     db: Session = Depends(get_db),
+# ):
+#     new_quantity = crud.add_stock(db, product_id=product_id, branch_id=branch_id, amount=amount)
+#     return {"branch_id": branch_id, "product_id": product_id, "quantity": new_quantity}
