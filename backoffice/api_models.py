6fb2f125ac2ff5a1d4e93a8cd4d00c9445fefb3a
Laurent Lacôte <laurent.lacote@private.com>
2026-07-27 23:00:00 +0200

    Feat: Backoffice - Backoffice API #8 #9
    IMPORTANT: are missing
    - Endpoints related to stocks for Managers in UI
    - Integration with HTML files.
    - Exposition of UX for Managers and Admin as a server.
    - Many tests.
    
    CHANGED
    - Added "auth.py" to manage authentication and passwords,
        for now using bcrypt library.
    - Added CRUD methods for Users and Branch management
        (important: no "delete_users" method and
         no write methods for branches because explicitely
         out of scope of project's business requirements).
    - Created a backoffice_api.py to hold API methods
        and (later) the UI exposition.
    - Adjusted the API models as needed.
    - Adjusted the example environment file.
    - Exposes the Backoffice API as a Docker service
       (specific docker file + composer adjustements).
    - Adjusted requirements.
    - Added related tests.

--- backoffice/api_models.py
+++ backoffice/api_models.py
@@ -2,6 +2,7 @@
 # Previously called schemas.py
 
 from pydantic import BaseModel, ConfigDict, Field
+from db_models import UserRole
 
 # ===== API models used for "GET" requests =====
 
@@ -27,3 +28,38 @@ class BranchOut(BaseModel):
 
     id: int
     label: str
+
+
+class UserOut(BaseModel):
+    """UserOut as in user outputed for API consumption"""
+
+    # "Helper class" mapping automatically this class attributes
+    #    to ones defined in a ORM Model defined from SQLAlchemy library.
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int
+    name: str
+    role: UserRole
+    branch_id: int | None
+    is_active: bool
+    # Password_hash is left out on purpose for security reasons.
+
+
+# ===== API models used for "POST" requests =====
+
+class LoginRequest(BaseModel):
+    username: str
+    password: str
+
+
+class PasswordReset(BaseModel):
+    """Just a Data Transfert Object to ease up new password retrieval"""
+    # new_password: str  # Not secure enough to my taste (would accept "")
+    new_password: str = Field(min_length=8, max_length=72)
+    # Max_length because for now we use bcrypt
+    # Field is a class allowing to specify data validation constraints
+    #   upon the data type.
+
+
+class BranchAssignment(BaseModel):
+    branch_id: int = Field(gt=0)
--- backoffice/crud.py
+++ backoffice/crud.py
@@ -4,6 +4,9 @@ from sqlalchemy.orm import Session
 from sqlalchemy import select, update
 from sqlalchemy.dialects.mysql import insert as mysql_insert
 from db_models import Stock
+from db_models import User, UserRole
+from db_models import Branch
+from sqlalchemy.orm import selectinload
 
 # ========================= STOCK RELATED CRUD =========================
 def get_stock(db: Session, *, product_id: int, branch_id: int) -> Stock | None:
@@ -85,3 +88,90 @@ def delete_stock(db: Session, *, product_id: int, branch_id: int) -> bool:
     )
     db.execute(stmt)
     db.commit()
+
+# ========================= USERS RELATED CRUD =========================
+def list_users(db: Session):
+    """Returns as a list of ORM Users all users in database"""
+    ls_usrs_stmt = select(User).order_by(User.id, User.role)
+    users_list = db.scalars(ls_usrs_stmt).all()
+    return users_list
+
+
+def create_user(
+    db: Session, user_name: str, pwd_hash: str,
+    role: UserRole = UserRole.MANAGER, branch_id: int | None = None
+) -> User:
+    new_user = User(
+        name=user_name,
+        password_hash=pwd_hash,
+        role=role,
+        branch_id=branch_id,
+        is_active=True
+    )
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+
+def get_user_by_name(db: Session, user_name: str):
+    """Returning the one user which user_name matches input
+        Used by login
+    """
+    find_usr_stmt = select(User).where(User.name == user_name)
+    matching_usr = db.scalars(find_usr_stmt).first()
+    return matching_usr
+
+
+def reset_user_password(db: Session, *, user_id: int, password_hash: str) -> bool:
+    """Resets a given user's password, identified by its id"""
+    result = db.execute(
+        update(User).where(User.id == user_id).values(password_hash=password_hash)
+    )
+    db.commit()
+    return result.rowcount > 0
+
+
+def assign_branch(db: Session, *, user_id: int, branch_id: int) -> bool:
+    """Changes the branch a user is associated with IF user is Manager"""
+    result = db.execute(
+        update(User)
+        .where(User.id == user_id, User.role == UserRole.MANAGER)
+        .values(branch_id=branch_id)
+    )
+    db.commit()
+    return result.rowcount > 0
+
+
+def set_user_active_state(db: Session, *, user_id: int, is_active: bool) -> bool:
+    result = db.execute(
+        update(User).where(User.id == user_id).values(is_active=is_active)
+    )
+    db.commit()
+    return result.rowcount > 0
+
+
+# ========================= BRANCHES RELATED CRUD =========================
+def list_branches(db: Session) -> list[Branch]:
+    all_branches_infos_stmt = select(Branch)
+    return db.scalars(all_branches_infos_stmt).all()
+
+
+def list_branches_ordered_by_label(db: Session):
+    ordered_branches_stmt = select(Branch).order_by(Branch.label)
+    return db.scalars(ordered_branches_stmt).all()
+
+
+def get_branches_with_active_managers(db: Session):
+    stmt = select(Branch).options(selectinload(Branch.managers)).order_by(Branch.label)
+    return db.scalars(stmt).all()
+
+
+def find_branches_by_name(db: Session, *, search_string: str) -> List[Branch]:
+    """Returns branches which label 'has' search_string, ordered by label"""
+    matching_branches_stmt = (
+        select(Branch)
+        .where(Branch.label.ilike(f"%{search_string}%"))
+        .order_by(Branch.label)
+    )
+    return list(db.scalars(matching_branches_stmt).all())
