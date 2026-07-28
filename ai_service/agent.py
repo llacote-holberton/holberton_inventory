d13f8bfe49af2639639a56f5ad3269cb9ff66cf7
Laurent Lacôte <laurent.lacote@private.com>
2026-07-28 18:15:16 +0200

    Fix: AI Service - Still incorrect MCP Toolset import #12
    Still mismatch between import and use (MCP vs Mcp)
    -> Used an alias on import to align the easiest way.

--- ai_service/agent.py
+++ ai_service/agent.py
@@ -6,7 +6,7 @@ from google.adk.agents import LlmAgent
 from google.adk.models.lite_llm import LiteLlm
 from google.adk.runners import Runner
 from google.adk.sessions import InMemorySessionService
-from google.adk.tools.mcp_tool.mcp_toolset import MCPToolset
+from google.adk.tools.mcp_tool.mcp_toolset import MCPToolset as McpToolset
 from google.adk.tools.mcp_tool import StreamableHTTPConnectionParams
 from google.genai import types
 
